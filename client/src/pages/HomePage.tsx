ee2333a0b7758fac21d32deba052bedbcdac0bce
abdul alim <alimsujon12@gmail.com>
2025-02-04 17:54:16 +0600

    latest articles and tips section done

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,13 +1,15 @@
 import BestServices from "../section/Home/BestServices"
+import CustomerSay from "../section/Home/CustomerSay"
 import Hero from "../section/Home/Hero"
 import LatestArticle from "../section/Home/LatestArticle"
 
 const HomePage = () => {
   return (
-    <div>
+    <div className="w-full overflow-hidden mx-auto">
       <Hero/>
       <BestServices />
       <LatestArticle/>
+      <CustomerSay/>
     </div>
   )
 }
---
+++ client/src/section/Home/ArticleCard.tsx
@@ -0,0 +1,37 @@
+
+
+const ArticleCard = () => {
+    return (
+      <div className="w-[315px] min-h-[561px] flex flex-col gap-[26px] mr-5 ">
+<div className="w-full max-w-[315px] h-[398px] flex flex-col gap-[10px] bg-gray-400">
+{/* this is image */}
+</div>
+<div className="w-[293px] min-h-[137px] flex flex-col gap-[24px]">
+
+<div className="w-full max-w-[293px] min-h-[87px] flex flex-col gap-[9px]">
+    <div className="max-w-[293px] min-h-[26px] self-start">
+    <h1>GYMNASTIC</h1>
+    </div>
+    <div className="max-w-[293px] min-h-[52px] ">
+        
+        <p className="font-inter font-bold text-[20px] leading-[26.4px] tracking-[-0.01em] text-[#6636EE]">
+        WORKOUT CONDITION FOR ALL
+        AROUND FITNESS
+       </p>
+    </div>
+    <div className="w-[273px] h-[26px] flex  gap-[25px] items-center">
+      <div className="">OCTOBER 4, 2022</div>
+      <div className="">O COMMENTS</div>
+</div>
+  
+</div>
+   
+ 
+</div>
+
+
+      </div>
+    )
+  }
+  
+  export default ArticleCard;
