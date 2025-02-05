854328e65e8ffea3036d7b86f1c547c58cec712e
EngrArfin <arfin.cse.green.edu.bd@gmail.com>
2025-02-05 10:46:15 +0600

    all fille update

--- client/src/section/Home/WickendList.tsx
+++ client/src/section/Home/WickendList.tsx
@@ -2,6 +2,11 @@ const WickendList = () => {
   return (
     <div>
       <h1>demo</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+        officiis nam explicabo, voluptate excepturi a id voluptas non quisquam
+        sunt. Itaque tenetur dolorem vel ipsum culpa, ab alias velit iure.
+      </p>
     </div>
   );
 };
