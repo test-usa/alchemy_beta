965d04d7726e779935eb632a89f58bff080d204e
Md Bakhtiar Fahim <bakhtiarfahim360@gmail.com>
2025-02-07 17:54:34 +0600

    Added nutritino page 1

--- client/src/section/nutrition/IntermediateFasting.tsx
+++ client/src/section/nutrition/IntermediateFasting.tsx
@@ -1,9 +1,101 @@
-import React from 'react'
+import { HeartPulse } from "lucide-react"
 
 const IntermediateFasting = () => {
-  return (
-    <div>IntermediateFasting</div>
-  )
+    return (
+        <div className="mb-10">
+            <img className='w-full rounded max-w-2xl' src="https://img.freepik.com/free-photo/young-guy-aged-man-sitting-sofa_23-2148030251.jpg?t=st=1738925315~exp=1738928915~hmac=dc57c81d65b94f934dd739ec8680131abc5c14430a0cd79e527c173d57557cad&w=1380" alt="" />
+
+            <h2 className='text-xl font-semibold mt-3 mb-2'>Intermittent Fasting</h2>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae distinctio reprehenderit quae repellendus consequuntur est eaque. Debitis, dignissimos blanditiis? Odit neque labore voluptatem nesciunt ex amet atque doloremque quam vel laudantium quas, temporibus accusamus sapiente consectetur, totam veniam deserunt possimus!</p>
+            <p>neque labore voluptatem nesciunt ex amet atque doloremque quam vel laudantium quas, temporibus accusamus sapiente consectetur, totam veniam deserunt possimus!</p>
+            <p className="border-b-2 border-b-gray-300 mt-4 mb-2"></p>
+
+            <div className="grid grid-cols-3 gap-4 my-5">
+                <img
+                    src="https://img.freepik.com/premium-photo/pan-food-with-chicken-tomatoes-peppers_1126714-32814.jpg?w=826"
+                    alt=""
+                    className="w-full rounded"
+                />
+                <img
+                    src="https://img.freepik.com/premium-photo/plate-food-with-rice-chickenbeef-vegetables_862330-25220.jpg?w=826"
+                    alt=""
+                    className="w-full rounded"
+                />
+                <img
+                    src="https://img.freepik.com/free-photo/top-view-chicken-sac-with-fried-potatoes-tomato-lavash-smoke_176474-3316.jpg?t=st=1738926237~exp=1738929837~hmac=679f9b45bd2c88693cd5d18e93dd8114e307d898e316004d3566a000be649f89&w=826"
+                    alt=""
+                    className="w-full rounded"
+                />
+            </div>
+
+            <h2 className='text-xl font-semibold mt-3 mb-2'>What we are Best?</h2>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae distinctio reprehenderit quae repellendus consequuntur est eaque. Debitis, dignissimos blanditiis? Odit neque labore voluptatem nesciunt onsectetur, totam veniam deserunt possimus!</p>
+
+
+            <div className="grid grid-cols-2 gap-10 mt-5">
+                <div className="flex items-center gap-5">
+                    <div className="flex items-center justify-center rounded-full text-[#6636EE]  border border-gray-300 p-3">
+                        <HeartPulse />
+                    </div>
+                    <div>
+                        <h4 className="text-[#6636EE] font-semibold text-lg mb-2">Heart Healthy</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, praesentium?</p>
+                    </div>
+                </div>
+                <div className="flex items-center gap-5">
+                    <div className="flex items-center justify-center rounded-full text-[#6636EE]  border border-gray-300 p-3">
+                        <HeartPulse />
+                    </div>
+                    <div>
+                        <h4 className="text-[#6636EE] font-semibold text-lg mb-2">Heart Healthy</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, praesentium?</p>
+                    </div>
+                </div>
+                <div className="flex items-center gap-5">
+                    <div className="flex items-center justify-center rounded-full text-[#6636EE]  border border-gray-300 p-3">
+                        <HeartPulse />
+                    </div>
+                    <div>
+                        <h4 className="text-[#6636EE] font-semibold text-lg mb-2">Heart Healthy</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, praesentium?</p>
+                    </div>
+                </div>
+                <div className="flex items-center gap-5">
+                    <div className="flex items-center justify-center rounded-full text-[#6636EE]  border border-gray-300 p-3">
+                        <HeartPulse />
+                    </div>
+                    <div>
+                        <h4 className="text-[#6636EE] font-semibold text-lg mb-2">Heart Healthy</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, praesentium?</p>
+                    </div>
+                </div>
+            </div>
+
+
+            <h2 className='text-xl font-semibold mt-3 mb-2 mt-5'>Our popular nutritionist</h2>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae distinctio reprehenderit quae repellendus consequuntur est eaque. Debitis, dignissimos blanditiis? Odit neque labore voluptatem nesciunt onsectetur, totam veniam deserunt possimus!</p>
+
+            <div className="flex justify-between items-center gap-5 mt-5">
+                <div className="w-full flex flex-col items-center justify-center gap-2 shadow-lg p-5 rounded">
+                    <img className="rounded-full h-16 md:h-20 lg:h-24 w-16 md:w-20 lg:w-24 object-cover border-[10px] border-gray-200 my-5" src="https://img.freepik.com/free-photo/smart-looking-teacher_53876-23045.jpg?t=st=1738928725~exp=1738932325~hmac=d7b7d59b939028b2524b0f025dd21a9d3124c5c0201950c475833b9c68be41bf&w=900" alt="" />
+                    <h4 className="text-lg font-semibold">Frances Swan</h4>
+                    <p className="text-zinc-400">Rainbow Bay crafts</p>
+                </div>
+                <div className="w-full flex flex-col items-center justify-center gap-2 shadow-lg p-5 rounded">
+                    <img className="rounded-full h-16 md:h-20 lg:h-24 w-16 md:w-20 lg:w-24 object-cover border-[10px] border-gray-200 my-5" src="https://img.freepik.com/free-photo/smart-looking-teacher_53876-23045.jpg?t=st=1738928725~exp=1738932325~hmac=d7b7d59b939028b2524b0f025dd21a9d3124c5c0201950c475833b9c68be41bf&w=900" alt="" />
+                    <h4 className="text-lg font-semibold">Frances Swan</h4>
+                    <p className="text-zinc-400">Rainbow Bay crafts</p>
+                </div>
+                <div className="w-full flex flex-col items-center justify-center gap-2 shadow-lg p-5 rounded">
+                    <img className="rounded-full h-16 md:h-20 lg:h-24 w-16 md:w-20 lg:w-24 object-cover border-[10px] border-gray-200 my-5" src="https://img.freepik.com/free-photo/smart-looking-teacher_53876-23045.jpg?t=st=1738928725~exp=1738932325~hmac=d7b7d59b939028b2524b0f025dd21a9d3124c5c0201950c475833b9c68be41bf&w=900" alt="" />
+                    <h4 className="text-lg font-semibold">Frances Swan</h4>
+                    <p className="text-zinc-400">Rainbow Bay crafts</p>
+                </div>
+            </div>
+
+
+        </div>
+    )
 }
 
 export default IntermediateFasting
