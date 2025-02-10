333634c58c4667500d32df98bac25d1d8edf4c61
Rahman Abdul Quadir <rahmanaq777@gmail.com>
2025-02-10 16:20:30 +0600

    pulled from others

--- client/src/section/Home/Badge.tsx
+++ client/src/section/Home/Badge.tsx
@@ -5,7 +5,7 @@ const Badge = () => {
     <div className="lg:pt-5 pb-20 mx-10">
       <div className="text-[#A1A1A1]">
         <div className="w-full flex justify-center">
-          <ul className="flex flex-wrap md:flex-nowrap gap-5 sm:gap-7 w-full max-w-[850px] justify-evenly border border-gradient-border mt-8 px-6 sm:px-12 md:px-[119px] py-6 sm:py-8 md:py-[39px] rounded-full">
+          <ul className="flex flex-wrap md:flex-nowrap gap-5 sm:gap-7 w-full max-w-[850px] justify-around border border-gradient-border mt-8 px-6 sm:px-12 md:px-[119px] py-6 sm:py-8 md:py-[39px] rounded-full">
             <li>
               <FaRunning className="w-[50px] h-[50px] sm:w-[60px] sm:h-[60px] md:w-[76px] md:h-[76px] text-[#A1A1A1]" />
             </li>
--- client/src/section/Shop/AllProducts.tsx
+++ client/src/section/Shop/AllProducts.tsx
@@ -12,12 +12,10 @@ export interface IProduct {
   category: string;
   tags: string[];
   quantity: number;
-  createdAt: string; // ISO date string
-  updatedAt: string; // ISO date string
+  createdAt: string;
+  updatedAt: string;
   __v: number;
 }
-
-
 
 const fetchShopItems = async () => {
   const response = await axios.get(
@@ -32,54 +30,41 @@ export const AllProducts = () => {
     queryFn: fetchShopItems,
   });
 
-  console.log(data, error);
   if (isLoading)
     return (
-      <div className="flex flex-row justify-center gap-x-16 items-center">
-        <div className="space-y-2">
-          <Skeleton className="h-[325px] w-[250px] rounded-xl" />
-          <Skeleton className="h-4 w-[250px]" />
-          <Skeleton className="h-4 w-[200px]" />
-        </div>
-        <div className="space-y-2">
-          <Skeleton className="h-[325px] w-[250px] rounded-xl" />
-          <Skeleton className="h-4 w-[250px]" />
-          <Skeleton className="h-4 w-[200px]" />
-        </div>
-        <div className="space-y-2">
-          <Skeleton className="h-[325px] w-[250px] rounded-xl" />
-          <Skeleton className="h-4 w-[250px]" />
-          <Skeleton className="h-4 w-[200px]" />
-        </div>
-        <div className="space-y-2">
-          <Skeleton className="h-[325px] w-[250px] rounded-xl" />
-          <Skeleton className="h-4 w-[250px]" />
-          <Skeleton className="h-4 w-[200px]" />
-        </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 px-4 sm:px-6 lg:px-8">
+        {Array.from({ length: 8 }).map((_, index) => (
+          <div key={index} className="space-y-2">
+            <Skeleton className="h-[325px] w-full rounded-xl" />
+            <Skeleton className="h-4 w-full" />
+            <Skeleton className="h-4 w-3/4" />
+          </div>
+        ))}
       </div>
     );
-  if (error) return <p>Error: {error.message}</p>;
+
+  if (error) return <p className="text-center text-red-500">Error: {error.message}</p>;
 
   return (
-    <div className="w-[1320px] mx-auto">
+    <div className="max-w-screen-xl w-full mx-auto px-4 sm:px-6 lg:px-8">
       {/* Product Grid */}
-      <div className="px-6 pb-12">
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-          {data.map((product: IProduct, index: number) => (
-            <div key={index} className="w-[315px] rounded-xl p-4">
-              <img
-                src={product.image}
-                alt={product.name}
-                className="w-[315px] h-[398px] object-cover rounded-sm"
-              />
-              <h3 className="text-lg font-semibold mt-4">{product.name}</h3>
-              <p className="text-gray-600">${product.price}</p>
-              <Link to={`/shop-details/${product._id}`}>
-                <button className="w-[113px] h-[48px] bg-primary text-white py-2 mt-4 rounded-sm transition gap-y-[24px]">
-                  Buy Now
-                </button>
-              </Link>
-            </div>
+      <div className="pb-12">
+        <div className="grid grid-cols-1  md:grid-cols-2  xl:grid-cols-4 gap-4 place-items-center">
+          {data.map((product: IProduct) => (
+           <div key={product._id} className="w-[300px] rounded-sm p-4">
+           <img
+             src={product.image}
+             alt={product.name}
+             className="w-[300px] h-[378px] object-cover rounded-sm"
+           />
+           <h3 className="text-lg font-semibold mt-4">{product.name}</h3>
+           <p className="text-gray-600">${product.price}</p>
+           <Link to={`/shop-details/${product._id}`}>
+             <button className="w-[113px] h-[48px] bg-primary text-white py-2 mt-4 rounded-sm transition gap-y-[24px]">
+               Buy Now
+             </button>
+           </Link>
+         </div>
           ))}
         </div>
       </div>
