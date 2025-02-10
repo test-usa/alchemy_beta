96e21ff5bb50ccbc12908f99747f67da1ef0d4a1
Rahman Abdul Quadir <rahmanaq777@gmail.com>
2025-02-10 11:03:06 +0600

    home page responsiveness issue fixed

--- client/src/section/Home/LatestArticle.tsx
+++ client/src/section/Home/LatestArticle.tsx
@@ -17,7 +17,7 @@ const LatestArticle = () => {
         </div>
       </div>
       <div className="mt-10 flex justify-center">
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        <div className="grid lg:grid-cols-4  md:grid-cols-2 sm:grid-cols-1 gap-6">
           <ArticleCard />
           <ArticleCard />
           <ArticleCard />
--- client/src/shared/MidNavCom.tsx
+++ client/src/shared/MidNavCom.tsx
@@ -1,41 +1,23 @@
-import React from "react";
 
 import { MdStarRate } from "react-icons/md";
 
 const MidNavCom = () => {
   return (
-    <div className="w-full max-w-[1920px] mx-auto text-white bg-primary flex flex-wrap md:flex-nowrap justify-around items-center px-4 md:px-6 shadow-md border-b  md:h-[80px] py-4 md:py-0">
-      <div className="flex items-center gap-2 md:gap-4 mb-4 md:mb-0">
-        <p className="text-xl font-bold">
-          <MdStarRate className="w-6 h-6 md:w-[28px] md:h-[28px]" />
-        </p>
-        <h1 className="text-lg md:text-[32px] font-bold text-white leading-tight md:leading-[42.24px] tracking-tight md:tracking-[-0.02em]">
-          FITNESS AND GYM
-        </h1>
-      </div>
-      <div className="flex items-center gap-2 md:gap-4 mb-4 md:mb-0">
-        <p className="text-xl font-bold">
-          <MdStarRate className="w-6 h-6 md:w-[28px] md:h-[28px]" />
-        </p>
-        <h1 className="text-lg md:text-[32px] font-bold text-white leading-tight md:leading-[42.24px] tracking-tight md:tracking-[-0.02em]">
-          MEDITATION
-        </h1>
-      </div>
-      <div className="flex items-center gap-2 md:gap-4 mb-4 md:mb-0">
-        <p className="text-xl font-bold">
-          <MdStarRate className="w-6 h-6 md:w-[28px] md:h-[28px]" />
-        </p>
-        <h1 className="text-lg md:text-[32px] font-bold text-white leading-tight md:leading-[42.24px] tracking-tight md:tracking-[-0.02em]">
-          TRACKER
-        </h1>
-      </div>
-      <div className="flex items-center gap-2 md:gap-4 mb-4 md:mb-0">
-        <p className="text-xl font-bold">
-          <MdStarRate className="w-6 h-6 md:w-[28px] md:h-[28px]" />
-        </p>
-        <h1 className="text-lg md:text-[32px] font-bold text-white leading-tight md:leading-[42.24px] tracking-tight md:tracking-[-0.02em]">
-          NUTRITION
-        </h1>
+    <div className="w-full max-w-[1920px] mx-auto text-white bg-primary shadow-md border-b px-4 md:px-6 py-4">
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-6 md:gap-4 items-center justify-items-center">
+        {[
+          "FITNESS AND GYM",
+          "MEDITATION",
+          "TRACKER",
+          "NUTRITION",
+        ].map((text, index) => (
+          <div key={index} className="flex items-center justify- gap-2 md:gap-4">
+            <MdStarRate className="w-6 h-6 md:w-[28px] md:h-[28px]" />
+            <h1 className="text-sm md:text-[32px]  font-bold leading-tight md:leading-[42.24px] tracking-tight md:tracking-[-0.02em]">
+              {text}
+            </h1>
+          </div>
+        ))}
       </div>
     </div>
   );
