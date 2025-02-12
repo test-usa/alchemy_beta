278244767a4dc7f8c17967fe088f03dcd52c008c
EngrArfin <arfin.cse.green.edu.bd@gmail.com>
2025-02-12 10:36:35 +0600

    yogaga

--- client/src/section/Workout/Yoga.tsx
+++ client/src/section/Workout/Yoga.tsx
@@ -2,7 +2,8 @@
 const Yoga = () => {
     return (
         <div>
-            <h1>Yoga Page </h1>
+            <h1>Yoga Page  page here more info</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti adipisci dolorum magni reiciendis fugit possimus magnam rerum nam esse odit ex laboriosam velit saepe, voluptatum error harum ut commodi dolore.</p>
 
         </div>
     );
